59e0ab73f6ca25d4c7d376a7abc8f9c748ea0dba
Uruzmag15 <note4xacc@gmail.com>
2017-07-04 00:49:36 +0300

    gunicorn

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
-sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
+sudo ln -s /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start﻿
