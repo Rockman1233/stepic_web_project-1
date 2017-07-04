5c3198e52ebb120c62715e35bec05c33a7d370e6
Uruzmag15 <note4xacc@gmail.com>
2017-07-04 10:33:09 +0300

    gunicorn

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def wsgi_application(environ, start_response):
+def app(environ, start_response):
     status = '200 OK'
     body = [bytes(v + '\n') for v in environ['QUERY_STRING'].split('&')]
     headers = [('Content-Type',  'text/plain')]
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start﻿
