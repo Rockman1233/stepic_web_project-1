2b4d8b81b8b0bc5b44e0cf6a286918a065f2ce18
Uruzmag15 <note4xacc@gmail.com>
2017-07-09 18:39:24 +0300

    django

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='test'),
+    url(r'^login/', views.test, name='test'),                                   
+    url(r'^signup/', views.test, name='test'),                                  
+    url(r'^question/\d+/', views.test, name='test'),                            
+    url(r'^ask/', views.test, name='test'),                                     
+    url(r'^popular/', views.test, name='test'),                                 
+    url(r'^new/', views.test, name='test'),
 ]
