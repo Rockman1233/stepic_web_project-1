efc9e6f64451deb7eed845d37211beb0e94f27dc
Uruzmag15 <note4xacc@gmail.com>
2017-07-11 13:08:01 +0300

    django

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test_dj
+sudo ln -s /home/box/web/etc/gunicorndjango.conf /etc/gunicorn.d/testdj
 sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
 #sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
