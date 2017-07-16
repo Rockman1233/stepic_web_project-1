deafa0125f23ea6e03b6d861a3bae1af74811002
Uruzmag15 <note4xacc@gmail.com>
2017-07-16 22:00:29 +0300

    2.3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class QuestionManager(models.Model):
+class QuestionManager(models.Manager):
 	def new():
 		pass
 	def popular():
