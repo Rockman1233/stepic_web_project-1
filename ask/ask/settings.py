54fa5e7b4507b172d12fdc5b63003e1d17eea44a
Uruzmag15 <note4xacc@gmail.com>
2017-07-22 10:32:52 +0300

    2.5

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qabase',
+	'USER': 'qauser',
+	'PASSWORD': '123456',
+	'HOST': '',
+	'PORT': '3306',
     }
 }
 
